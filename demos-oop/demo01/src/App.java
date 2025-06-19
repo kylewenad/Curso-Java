e4cee079291046952a0505979272eabb37cf0af0
kylewenad <kylewen@hotmail.com>
2025-06-19 11:04:51 +0200

    add person

--- demos-oop/demo01/src/App.java
+++ demos-oop/demo01/src/App.java
@@ -1,5 +1,22 @@
+import base.Person;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        Person person1 = new Person();
+        //importa la clase Person
+        //instanciar 'crear'personas
+
+            person1.name = "Pepe";
+            person1.age = 22;
+            person1.greetings();
+
+        Person juan = new Person ();
+        
+            juan.name = "Juan";
+            juan.age = 25;
+            juan.greetings();
+
+
     }
 }
---
+++ demos-oop/demo01/src/base/Person.java
@@ -0,0 +1,12 @@
+package base;
+
+public class Person {
+
+    public String name;
+    public int age;
+      
+    public void greetings() {
+        System.out.printf("Hola soy %s y tengo %s años.\n", name, age);
+      }  
+
+}
