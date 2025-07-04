9e007c90016a5ac3d842193cbd68bcbc904f0660
kylewenad <kylewen@hotmail.com>
2025-07-04 13:54:13 +0200

    more 4 july

--- demo-concepts/src/test/java/local/concept2_vc/CalcControllerTest.java
+++ demo-concepts/src/test/java/local/concept2_vc/CalcControllerTest.java
@@ -1,7 +1,9 @@
 package local.concept2_vc;
-
+//test para detectar los cornerCase (casos raros)
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -25,7 +27,6 @@ public class CalcControllerTest {
         assertEquals(0,calculator.add());
         //(lo que yo espero, resultado)
     }
-
     @Test
     public void testAddOk(){
         //5+3
@@ -36,7 +37,6 @@ public class CalcControllerTest {
         int result = calculator.add();
         assertEquals(8, result);
     }
-
     @Test
     public void testAddNegatives(){
         //-5+7
@@ -54,7 +54,6 @@ public class CalcControllerTest {
         CalcController subtract = new CalcController();
         assertEquals(0, subtract.subtract());
     }
-
     @Test
     public void testSubtract() {
         //7-3
@@ -66,7 +65,6 @@ public class CalcControllerTest {
         int result = calculator.subtract();
         assertEquals(4, result);
     }
-
     @Test
     public void testSubtractNegatives() {
         //7-3
@@ -84,7 +82,6 @@ public class CalcControllerTest {
         //0*0
         assertEquals(0, 0);
     }
-
     @Test
     public void testMultiply() {
         //7*3
@@ -96,7 +93,6 @@ public class CalcControllerTest {
         int result = calculator.multiply();
         assertEquals(21, result);
     }
-
     @Test
     public void testMultiplyNegatives() {
         //7*3
@@ -116,8 +112,28 @@ public class CalcControllerTest {
     
     
     @Test
-    public void testCalculateFactorial() {
-        
+    public void testCalculateFactorialDefault() throws BusinessException {
+        // 0
+        assertEquals(1, calculator.calculateFactorial());
+    }
+
+    @Test
+    public void testCalculateFactorial() throws BusinessException {
+        // 5
+        calculator.setNum1(5);
+        assertEquals(120, calculator.calculateFactorial());
+    }
+
+    @Test
+    public void testCalculateFactorialNegatives() {
+        // -3
+        int num1 = -3;
+        try {
+            calculator.calculateFactorial();
+            fail("Situación no contemplada");
+        } catch (BusinessException e) {
+            assertTrue(e.getMessage().contains(ErrorCodes.ERROR_NEGATIVE.toString()));
+        }
     }
 
     @Test
@@ -126,19 +142,67 @@ public class CalcControllerTest {
     }
 
     @Test
-    public void testDecimalDivide() {
-     
+    public void testDecimalDivideDefault() throws BusinessException {
+        // 0/0
+        double result = calculator.decimalDivide();
+        assertEquals(Double.NaN, result, 1);
+    }
+
+    @Test
+    public void testDecimalDivideNoNan() throws BusinessException {
+        int num1;
+        int num2 = 0;
+        if (num2 == 0) {
+            throw new BusinessException(ErrorCodes.ERROR_ZERO, "Dividiendo por 0");
+        }
+        double result = calculator.decimalDivide();
+        assertEquals(Double.NaN, result, 1);
+    }
+
+    @Test
+    public void testDecimalDivide() throws BusinessException {
+        // 7/3
+        int num1 = 7;
+        int num2 = 3;
+        calculator = new CalcController();
+        calculator.setNum1(num1);
+        calculator.setNum2(num2);
+        double result = calculator.decimalDivide();
+        assertEquals(2.33, result, 0.01);
+        //delta es el margen de error del resultado de los decimales
     } 
 
     @Test
-    public void testIntDivide() {
+    public void testDecimalDivideNegatives() throws BusinessException {
+        // 7/3
+        int num1 = 7;
+        int num2 = -3;
+        calculator = new CalcController();
+        calculator.setNum1(num1);
+        calculator.setNum2(num2);
+        double result = calculator.decimalDivide();
+        assertEquals(-2.33, result, 0.01);
+        //delta es el margen de error del resultado de los decimales
+    } 
+
+    @Test
+    public void testIntDivideDefault() {
           // 0/0
         try {
             calculator.intDivide();
+            fail("Situación no contemplada");
         } catch (BusinessException e) {
-        BusinessException expected = new BusinessException(ErrorCodes.ERROR_ZERO, "Dividiendo por cero");
-        assertTrue(expected.equals(e));
+        //BusinessException expected = new BusinessException(ErrorCodes.ERROR_ZERO, "Dividiendo por cero");
+        //assertEquals(expected.getMessage(), e.getMessage());
+        assertTrue(true);
+        } catch (Exception e) {
+            fail("Excepción no soportada");
+            //un método fail al que nunca debe llegar, pero si llega estará el aviso
         }
+    }
+
+    public void intDivideLambdaTest() {
+
     }
 
 
@@ -147,7 +211,33 @@ public class CalcControllerTest {
         
     }
 
-
+    @Test
+    public void testIntDivide() throws BusinessException {
+        // 7/3
+        int num1 = 7;
+        int num2 = 3;
+        calculator = new CalcController();
+        calculator.setNum1(num1);
+        calculator.setNum2(num2);
+        int result = calculator.intDivide();
+        int result2 = calculator.restDivision();
+        assertEquals(2, result);
+        assertEquals(1, result2);
+    }
+    
+    @Test
+    public void testIntDivideNegatives() throws BusinessException {
+        //7*3
+        int num1 = 7;
+        int num2 = -3;
+        calculator = new CalcController();
+        calculator.setNum1(num1);
+        calculator.setNum2(num2);
+        int result = calculator.intDivide();
+        int result2 = calculator.restDivision();
+        assertEquals(-2, result);
+        assertEquals(1, result2);
+    }
 
 
 }
