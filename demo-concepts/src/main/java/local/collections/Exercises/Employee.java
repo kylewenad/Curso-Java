fecb808296a05f9128498362979284d4f46d83d8
kylewenad <kylewen@hotmail.com>
2025-07-19 13:10:10 +0200

    Finished Ex03 Collections

--- demo-concepts/src/main/java/local/collections/Exercises/Employee.java
+++ demo-concepts/src/main/java/local/collections/Exercises/Employee.java
@@ -1,50 +1,61 @@
 package local.collections.exercises;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.text.DateFormatter;
+
 //Defina una List con la clase Empleado antes definida
 //(con nombre, apellidos, fecha de contratación y número de empleado).
+
+//Esta ordenación se reflejará en un método de la clase Empleado, compareTo(obj o),
+//que permite comparar el objeto en curso (this) con el objeto recibido,
+//que devuelve -1 si es menor que el recibido, 0 si son iguales y 1 si es mayor que el recibido.
 public class Employee implements Comparable <Employee>{
-    List<String> employees = new ArrayList<>();
+    
     private String name;
     private String surname;
-    private LocalDate dateOfContraction;
-    private String employeeId;
+    private LocalDate date;
+    private int employeeId;
     
-    void Employee(String name, String surname, String dateOfContraction, String employeeNumber) {
+    public Employee(String name, String surname, String date, int employeeId) {
         this.name = name;
         this.surname = surname;
-        this.dateOfContraction = formatDate(dateOfContraction);
-        this.employeeId = employeeNumber;
+        this.date = formatDate(date);
+        this.employeeId = employeeId;
     }
 
-    private LocalDate formatDate(String dateOfContraction2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'formatDate'");
-    }
-
-    public static void main(String[] args) {
+    private LocalDate formatDate(String dateStr) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = LocalDate.parse(dateStr, formatter);
         
+        return date;
     }
-    
+
+    private long calculateEmployDays(){ //Para calcular los dias que lleva el empleado contratado
+        LocalDate actualDate = LocalDate.now(); //Fecha actual
+        return ChronoUnit.DAYS.between(date, actualDate); //Días que lleva en la empresa desde el día de contratación hasta la fecha actual
+    }
+
     @Override
     public String toString() {
         return 
-            "Employee [name=" + name + ", surname=" + surname + ", dateOfContraction=" + dateOfContraction
-            + ", employeeId=" + employeeId + "]";
+            "Employee:\n[name = " + name + 
+            ",\nsurname = " + surname + 
+            ",\ndateOfContraction = " + date
+            + ",\nemployeeId = " + employeeId + "]";
     }
 
     @Override
-    public int compareTo(Employee other) {
+    public int compareTo(Employee other) {//Compara un empleado con otro los días que llevan en la empresa
 
-        int diffDays = (int)(other.calculateEmployDays() - this.caculateEmployDays());
-        
+      int diffDays = (int)(other.calculateEmployDays() - this.calculateEmployDays());
         if (diffDays == 0) {
-            return this.id - other.id;
+            return this.employeeId - other.employeeId;
         }
-        return diffDays;
-        
+       return diffDays;
     }
 }
--- demo-concepts/src/main/java/local/collections/Exercises/Ex03.java
+++ demo-concepts/src/main/java/local/collections/Exercises/Ex03.java
@@ -2,19 +2,29 @@
 package local.collections.exercises;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 //Use la función sort de la clase Collections para ordenar la lista.
+//Use la antigüedad como criterio a seguir (es “mayor” si es más antiguo)
+//y si entraron en el mismo día, utilice el número de empleado para desempatar.
 public class Ex03 {
 
-    List<Employee> list = new ArrayList<>();
-    List<String> employees = new ArrayList<>();
-    
-    void addEmployees() {
-        employees.add("Amanda","Sanchez",10/10/2010,100);
-        
+    static List<Employee> listOfEmployees = new ArrayList<>();
 
+    static void sortList(){
+        Collections.sort(listOfEmployees);
     }
 
+    public static void main(String[] args) {
+        listOfEmployees.add(new Employee("Amanada", "Perez", "25/10/2010", 101));
+        listOfEmployees.add(new Employee("Juan", "Cuesta", "12/06/2008", 56));
+        listOfEmployees.add(new Employee("José Miguel", "Cuesta", "18/11/2020", 327));
+        listOfEmployees.add(new Employee("Natalia", "Cuesta", "18/11/2020", 326));
+        System.out.println(listOfEmployees);
+
+        sortList();
+        System.out.println("Lista ordenada de empleados: " + listOfEmployees);
+    }
 }
 
 
